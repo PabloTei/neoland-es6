cddb707fbbc6fa4755d8424686fe579c77b5d02f
PabloTei <pteijeirolopez@gmail.com>
2023-01-12 20:01:16 +0100

    iteracion1

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1.1 Ejecuta esta función sin pasar ningún parametro
+
+const getSum = () => {
+    return 10 + 5;
+}
+const sum = getSum();
+console.log(sum);
+
+// 1.2 Ejecuta esta función pasando un solo parametro
+
+const getSum1 = (a) => a + 5; 
+    
+const sum1 = getSum1(10);
+console.log(sum);
+
+// 1.3 Ejecuta esta función pasando dos parametros
+
+const getSum2 = (a, b) => a + b;
+
+const sum2 = getSum2 (10, 5);
+console.log(sum);
