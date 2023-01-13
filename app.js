0fde3731a9c93be8cff03feef327e7162730fbc4
PabloTei <pteijeirolopez@gmail.com>
2023-01-14 00:03:31 +0100

    iteration12

--- app.js
+++ app.js
@@ -142,16 +142,16 @@ console.log(nameFunction1(users1));
 // 4.3
 
 const cities = [
-	{isVisited:true, name: 'Tokyo'}, 
-	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Tokyo'},  
+	{isVisited:false, name: 'Madagascar'}, 
 	{isVisited:true, name: 'Amsterdam'}, 
 	{isVisited:false, name: 'Seul'}
 ];
 
 const citiesVisited = () => {
 
-const allCities = cities.map((city) => (city.isVisited ? `${city.name}: Visitado` : city.name))
-   return allCities;
+return cities.map((city) => (city.isVisited ? `${city.name}: Visitado` : city.name))
+
 }
 
 console.log(citiesVisited(cities));
