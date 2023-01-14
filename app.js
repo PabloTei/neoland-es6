d892e89e70a1072a3186a6c60ee6e2c5f8811966
PabloTei <pteijeirolopez@gmail.com>
2023-01-14 18:15:38 +0100

    iteration18

--- app.js
+++ app.js
@@ -301,3 +301,27 @@ const functionSumScore = () => {
     return sumScore;
 }
 console.log(functionSumScore());
+
+// 7.2
+
+const exams1 = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const functionSumScoreApproved = () => {
+
+    let alumnosAprobados = exams1.map((alumno) => alumno.score >= 5 ? alumno.score : alumno.score = 0);
+    alumnosAprobados = alumnosAprobados.reduce((acc, score) => acc + score, 0)
+    return alumnosAprobados;
+}
+console.log(functionSumScoreApproved());
+
